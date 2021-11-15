121a27e5442f9b383c67f2415a9c03bb057412e0
Thuc Nguyen <thnguyen@personalis.com>
2021-11-15 01:13:35 -0800

    Update firebase pipeline

--- public/app.js
+++ public/app.js
@@ -2,32 +2,32 @@ const auth = firebase.auth();
 
 function googleLogIn() {
 
-    const provider = new firebase.auth.GoogleAuthProvider();
+	const provider = new firebase.auth.GoogleAuthProvider();
 
-    auth.signInWithPopup(provider)
-        .then(result => {
-            const user = result.user;
-            console.log(user);
-        })
-        .catch(console.log);
+	auth.signInWithPopup(provider)
+		.then(result => {
+			const user = result.user;
+			console.log(user);
+		})
+		.catch(console.log);
 }
 
 function googleLogOut() {
-    auth.signOut()
-        .then( function() {
-            console.log("signed out")
-        })
-        .catch(console.log)
+	auth.signOut()
+		.then( function() {
+			console.log("signed out")
+		})
+		.catch(console.log)
 }
 
 auth.onAuthStateChanged(user => {
-    if (user) {
-        document.getElementById("whenSignedIn").hidden = false;
-        document.getElementById("whenSignedOut").hidden = true;
-        document.getElementById("userDetail").innerHTML = `<h3> "Hello"  ${user.displayName} !<h3> <p>User ID: ${user.uid} </p>`;
-    } else {
-        document.getElementById("whenSignedIn").hidden = true;
-        document.getElementById("whenSignedOut").hidden = false;
-        document.getElementById("userDetail").innerHTML = ``;
-    }
+	if (user) {
+		document.getElementById("whenSignedIn").hidden = false;
+		document.getElementById("whenSignedOut").hidden = true;
+		document.getElementById("userDetail").innerHTML = `<h3> "Hello"  ${user.displayName} !<h3> <p>User ID: ${user.uid} </p>`;
+	} else {
+		document.getElementById("whenSignedIn").hidden = true;
+		document.getElementById("whenSignedOut").hidden = false;
+		document.getElementById("userDetail").innerHTML = ``;
+	}
 })
