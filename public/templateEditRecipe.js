6d8cb95ac9c4e75862c22526d6f3c0b3666b4774
Fione Huynh <fhuynh@ucsd.edu>
2021-11-22 13:53:01 -0800

    Fixed edit recipe

--- public/templateEditRecipe.js
+++ public/templateEditRecipe.js
@@ -3,14 +3,23 @@ import {updatePrivateRecipe} from './backend/private_recipe.js';
 
 var title, img, calorie, descr, ings, instrucs; // maybe add cook time
 var obj = {};
+const data = localStorage['currentRecipeData'];
+const parsedData = JSON.parse(data);
+console.log(parsedData);
 
 function init(){
     let titleInput = document.getElementById('titleInput');
+    titleInput.value = parsedData.name;
     let imgUrlInput = document.getElementById('imgUrlInput');
+    imgUrlInput.value = parsedData.image_url;
     let calorieInput = document.getElementById('calorieInput');
+    calorieInput.value = parsedData.calories;
     let descriptionInput = document.getElementById('descriptionInput');
+    descriptionInput.value = parsedData.description;
     let ingredientsInput = document.getElementById('ingredientsInput');
+    ingredientsInput.value = parsedData.recipe_ingredient;
     let instructionsInput = document.getElementById('instructionsInput');
+    instructionsInput.value = parsedData.instructions;
     let saveButton = document.getElementById('saveButton');
 
     titleInput.addEventListener('input', function(){
@@ -39,8 +48,6 @@ function init(){
     });
 
     saveButton.addEventListener('click', function(){
-        const data = localStorage['currentRecipeData'];
-        const parsedData = JSON.parse(data);
         console.log("The JSON Object: " + JSON.stringify(obj));
         updatePrivateRecipe(parsedData.id, obj);
 
