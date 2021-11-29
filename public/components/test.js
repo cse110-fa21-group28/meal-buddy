3e8c1d6c0d0b44c0d19242aaca58d2fdf5e7b70f
hkim1016 <gimhan0813@gmail.com>
2021-11-29 09:31:18 -0800

    Update test.js

--- public/components/test.js
+++ public/components/test.js
@@ -1,9 +1,3 @@
-/**
- * Gets the sum of two numbers
- * @param {Number} a first operand
- * @param {Number} b second operand
- * @returns the sum of a + b
- */
 function sum(a, b) {
     return a + b;
 }
