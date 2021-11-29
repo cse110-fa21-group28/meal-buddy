b13a898c4ad858de0c13d424d8bb8902b7a4326c
hkim1016 <gimhan0813@gmail.com>
2021-11-29 09:03:10 -0800

    test file for jsdoc

---
+++ public/test.js
@@ -0,0 +1,9 @@
+/**
+ * Gets the sum of two numbers
+ * @param {Number} a first operand
+ * @param {Number} b second operand
+ * @returns the sum of a + b
+ */
+function sum(a, b) {
+    return a + b;
+}
