46e22bf672dc7914d7dc9cfd35caa46de62d36ab
Mingyu Li <mil074@ucsd.edu>
2021-11-29 18:04:29 -0800

    working update and delete functions

--- public/components/recipeCard.js
+++ public/components/recipeCard.js
@@ -103,7 +103,8 @@ class RecipeCard extends HTMLElement {
     // Button to edit recipe
     const editButton = document.createElement('button')
     editButton.innerText = 'Edit recipe'
-    editButton.addEventListener('click', () => {
+    editButton.addEventListener('click', (e) => {
+      e.stopPropagation()
       window.location.href = 'templateEditRecipe.html' // how to pass in the ID of the recipe?
       localStorage.setItem('currentRecipeData', JSON.stringify(data))
       // TODO: fetch the data that's already on the database, place it in the template textareas
@@ -114,11 +115,17 @@ class RecipeCard extends HTMLElement {
     // Button to delete recipe
     const deleteButton = document.createElement('button')
     deleteButton.innerText = 'Delete recipe'
-    deleteButton.addEventListener('click', () => {
+
+    deleteButton.addEventListener('click', (e) => {
       console.log('delete recipe clicked')
+      e.stopPropagation() 
       // TODO: go to the backend and delete the recipe
       deletePrivateRecipe(data.id)
-    })
+        // then(() => {
+        //   window.location.href = 'my-recipes.html'
+        // })
+      });
+      
 
     // Add all of the elements to the card
     card.appendChild(image)
