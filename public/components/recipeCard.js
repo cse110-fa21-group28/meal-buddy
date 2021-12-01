5af628dc59addc9efd0fa391b86acea4ab368e76
Derrick2000 <60729943+Derrick2000@users.noreply.github.com>
2021-11-30 16:54:45 -0800

    CD-8: Fixed recipe add issue (#29)
    * Fixed addRecipe issue
    
    * Apply callback for firebase function
    
    * CD 8/9 DONE

--- public/components/recipeCard.js
+++ public/components/recipeCard.js
@@ -117,15 +117,11 @@ class RecipeCard extends HTMLElement {
     deleteButton.innerText = 'Delete recipe'
 
     deleteButton.addEventListener('click', (e) => {
-      console.log('delete recipe clicked')
       e.stopPropagation()
-      // TODO: go to the backend and delete the recipe
-      deletePrivateRecipe(data.id)
-      setTimeout(() => {
-        location.reload(true);
-      }, 500);
+      firebase.firestore().collection('private_recipe').doc(data.id.toString()).delete().then(function () {
+        window.location.href = 'my-recipes.html'
+      })
     })
-    
 
     // Add all of the elements to the card
     card.appendChild(image)
